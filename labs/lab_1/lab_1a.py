daf3b13382b739a43eb866c9bdce6764ffa49363
Laggeding <andymapdx@gmail.com>
2026-02-25 00:01:55 -0800

    Update lab_1a.py
    Added Piazza post description

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Andy Ma" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"Hey everyone! My name is Andrew Ma, and I'm from Portland, OR. I love doing Earth/Environmental science research using computational methods. In my free time, I enjoy playing/watching tennis(Huge Novak fan!), and watching Christopher Nolan films. I'm also very interested in the huge startup scene down in SV with the AI-boom.")
 
 if __name__ == "__main__":
     main()
