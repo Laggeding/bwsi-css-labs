ef1c7f1102f716f3e33af89af9ff6dd7efe8301b
Laggeding <andymapdx@gmail.com>
2026-02-26 00:16:32 -0800

    Fixed calculation errors
    Keeps prompting until a valid operator/number is entered

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -8,6 +8,26 @@ The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
 """
+def clean_int(prompt: str) -> float:
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+
+        except ValueError:
+            print("Invalid input. Please try again")
+
+def clean_op(prompt: str) -> str:
+    ops = ["add", "subtract", "divide", "multiply"]
+    while True:
+        op = input(prompt).strip().lower()
+        if op in ops:
+            return op
+        else:
+            print("Invalid Operator. Please try again")
+
+        
+
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
@@ -33,18 +53,18 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
         if num2 != 0:
             return num1 / num2
         else:
-            raise ValueError("Cannot divide by zero.")
-    else:
-        raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
-
+            print("Cannot divide by zero.")
+            while (num2 == 0):
+                num2 = clean_int("Enter the second number: ")
+            return num1/num2
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    num1 = clean_int("Enter the first number: ")
+    num2 = clean_int("Enter the second number: ")
+    operation = clean_op("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
